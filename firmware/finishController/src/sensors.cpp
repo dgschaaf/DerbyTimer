cfd6b52768005273ff354aaa443d96722cb52c31
dgschaaf <darren.g.schaaf@gmail.com>
2026-05-21 17:44:43 -0400

    small cleanup on interrupt edge from compile test

--- firmware/finishController/src/sensors.cpp
+++ firmware/finishController/src/sensors.cpp
@@ -46,7 +46,7 @@ void armSensors(uint32_t raceStartMicros) {
     interrupts();
 
     // Attach interrupts on the configured edge.
-    int edge = config.activeHigh ? RISING : FALLING;
+    PinStatus edge = config.activeHigh ? RISING : FALLING;
     attachInterrupt(digitalPinToInterrupt(config.leftPin),  leftSensorISR,  edge);
     attachInterrupt(digitalPinToInterrupt(config.rightPin), rightSensorISR, edge);
 }
